446fdcc9d534405fa02aa3f8573ba2f0446e9582
laziccv <lazicv.git@gmail.com>
2024-05-11 11:47:31 +0200

    Created Queries, Controllers, Handlers folders and classes within.

---
+++ Code9.Domain/Queries/GetAllCinemaQuery.cs
@@ -0,0 +1,16 @@
+﻿using Code9.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code9.Domain.Queries
+{
+    public record GetAllCinemaQuery : IRequest<List<Cinema>>
+    {
+         
+    }
+}
